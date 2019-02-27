8561cfd4761b4cf4c983683c6c3c176d60ee7555
Uma Zalakain <uma@gisa-elkartea.org>
2019-02-27 20:45:03 +0000

    Get some good work done

--- src/Language/Netsim.hs
+++ src/Language/Netsim.hs
@@ -1,60 +1,161 @@
+{-# LANGUAGE DeriveGeneric, TupleSections #-}
+
 module Language.Netsim where
 
 import qualified Data.HashMap as M
-import Data.Maybe (mapMaybe)
-import Data.List (nub)
+import Data.Maybe (fromMaybe)
+import Data.Hashable (Hashable)
+import Text.Printf (printf)
+
+{-----------------}
+{- TYPE SYNONYMS -}
+{-----------------}
 
 type Node = String
-type Distance = Int
+type Source = Node
+type Destination = Node
+type Gateway = Node
+type Cost = Int
 
--- Link: ((N1, N2), Distance), where N1 < N2 lexicographically
-type Link = ((Node, Node), Distance)
+type DI a b c = M.Map a (M.Map b c)
 
--- Network links
-type Network = [Link]
+-- Static
+type Network = DI Source Destination Cost
 
--- Routing table: Destination -> (Distance, Gateway)
-type RTable = M.Map Node (Distance, Node)
+-- Dynamic
+type RTable = M.Map Destination (Cost, Gateway)
+type State = DI Source Destination (Cost, Gateway)
 
--- Network state: Node -> Routing table
-type State = M.Map Node RTable
+-- Messages
+type DV = M.Map Destination Cost
 
--- Directed advertisement: Destination -> Routing table
-type Advertisement = (Node, RTable)
+swap :: (Hashable a, Ord a, Hashable b, Ord b) => DI a b c -> DI b a c
+swap = M.unionsWith M.union
+     . M.elems
+     . M.mapWithKey (\n -> M.map (\a -> M.singleton n a))
 
--- For a given node and link, get destination and distance, if any
-nodeLink :: Node -> Link -> Maybe (Node, Distance)
-nodeLink n ((n1, n2), d)
-  | n == n1 = Just (n2, d)
-  | n == n2 = Just (n1, d)
-  | otherwise = Nothing
 
--- Get a routing table with just links to neighbours
-neighbourTable :: Node -> Network -> RTable
-neighbourTable n1
-  = M.fromList
-  . map (\(n2, d) -> (n2, (d, n1))) -- TODO: use arrows
-  . mapMaybe (nodeLink n1)
+{--------------------------}
+{- STRUCTURAL COMPUTATION -}
+{--------------------------}
 
--- Union of the rtable to neighbours and the rtable build so far
-rtable :: Node -> Network -> State -> RTable
-rtable n net st = neighbourTable n net `M.union` M.findWithDefault M.empty n st
+flatten :: DI a b c -> [(a, b, c)]
+flatten = concatMap (M.elems) . M.elems . M.mapWithKey (\a -> M.mapWithKey (\b c -> (a, b, c)))
 
--- Neighbour nodes for a given node
-neighbours :: Node -> Network -> [Node]
-neighbours n = M.keys . neighbourTable n
+unflatten :: (Hashable a, Ord a, Hashable b, Ord b) => [(a, b, c)] -> DI a b c
+unflatten = foldr (\(a, b, c) -> M.insertWith M.union a (M.singleton b c)) M.empty
 
--- Advertisement messages for a given node
-nodeAds :: Node -> Network -> State -> [Advertisement]
-nodeAds n net st = map (\n' -> (n', rtable n' net st)) (neighbours n net)
+bidir :: (Hashable a, Ord a) => DI a a b -> DI a a b
+bidir net = M.unionWith M.union net (swap net)
 
--- All nodes in a network
-nodes :: Network -> [Node]
-nodes = nub . concatMap (\((n1, n2), _) -> [n1, n2])
+ego :: Network -> Network
+ego = M.mapWithKey (\n -> M.insert n 0)
 
--- All advertisement messages in the network
-netAds :: Network -> State -> [Advertisement]
-netAds net st = concatMap (\n -> nodeAds n net st) (nodes net)
+network :: [(Node, Node, Cost)] -> Network
+network = ego . bidir . unflatten
 
-rtableUpdate :: RTable -> [RTable] -> RTable
-rtableUpdate = undefined
+{------------}
+{- PRINTING -}
+{------------}
+
+printNetwork :: Network -> String
+printNetwork = unlines
+             . map (\(s, d, c) -> printf "<%s, %s, %d>" s d c)
+             . flatten
+
+printState :: State -> String
+printState = unlines
+           . map (\(s, d, (c, g)) -> printf "<%s, %s, %d, %s>" s d c g)
+           . flatten
+
+{------------------}
+{- INITIALIZATION -}
+{------------------}
+
+-- Initial state: each node knows about its neighbours
+state :: Network -> State
+state = M.map (M.mapWithKey (\d c -> (c, d)))
+
+{---------------}
+{- ADVERTISING -}
+{---------------}
+
+type GetDV = Source -> Destination -> State -> DV
+
+-- Without split-horizon: no extra checks on the DVs sent out
+woSH :: GetDV
+woSH s _ = M.map fst . M.findWithDefault M.empty s
+
+-- With split-horizon: do not include d in the distance vector sent to d
+wSH :: GetDV
+wSH s d = M.filterWithKey (\d' c -> d' /= d) . woSH s d
+
+send :: GetDV -> State -> Network -> DI Source Destination DV
+send f st = M.mapWithKey (\s -> M.mapWithKey (\d _ -> f s d st))
+
+{------------}
+{- UPDATING -}
+{------------}
+
+merge :: RTable -> RTable -> RTable
+merge = M.unionWith min
+
+update :: Gateway -> DV -> RTable -> RTable
+update g dv rtable = case M.lookup g rtable of
+                       Nothing -> rtable
+                       Just (c, _) -> let rtable' = M.map ((, g) . (+ c)) dv
+                                       in merge rtable rtable'
+
+updateNode :: M.Map Source DV -> RTable -> RTable
+updateNode ads rtable = M.foldWithKey update rtable ads
+
+receive :: DI Destination Source DV -> State -> State
+receive ads = M.mapWithKey (\s -> updateNode (M.findWithDefault M.empty s ads))
+
+{-----------}
+{- RUNNING -}
+{-----------}
+
+tick :: GetDV -> Network -> State -> State
+tick f net st = let adsSent = send f st net
+                 in receive (swap adsSent) st
+
+run :: GetDV -> Network -> State -> [State]
+run f net st = let st' = tick f net st
+                in st : if st == st'
+                           then []
+                           else run f net st'
+
+runN :: GetDV -> Network -> Int -> State -> [State]
+runN f net n = take n . run f net
+
+{------------}
+{- EXAMPLES -}
+{------------}
+
+ex1 :: Network
+ex1 = network
+    [ ("alpha", "beta", 0)
+    , ("gamma", "beta", 1)
+    ]
+
+ex2 :: Network
+ex2 = network
+    [ ("n1", "n3", 6)
+    , ("n3", "n6", 1)
+    , ("n6", "n5", 2)
+    , ("n5", "n4", 1)
+    , ("n4", "n2", 1)
+    , ("n2", "n1", 3)
+    , ("n2", "n5", 3)
+    , ("n1", "n5", 1)
+    , ("n5", "n3", 3)
+    ]
+
+ex3 :: Network
+ex3 = network
+    [ ("A", "B", 3)
+    , ("A", "C", 23)
+    , ("B", "C", 2)
+    , ("C", "D", 5)
+    ]

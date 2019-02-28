299075c3fb292903f700572f4689602743e3ba1f
Uma Zalakain <uma@gisa-elkartea.org>
2019-02-28 15:57:30 +0000

    Clean up CLI

--- src/Main.hs
+++ src/Main.hs
@@ -9,22 +9,16 @@ import System.Exit (exitFailure, exitSuccess)
 
 import Language.Netsim
 
-
--- | Parses a network represented as a string
--- | Links must be split by newlines
--- | Each link must have format 'alpha beta cost' where 'cost' is an integer
-parseNetwork :: String -> Maybe [(Node, Node, Cost)]
-parseNetwork s = mapM (parseRow . words) (lines s)
-    where parseRow [a, b, c] = case readMaybe c :: Maybe Int of
-                                 Nothing -> Nothing
-                                 Just c' -> Just (a, b, c')
-          parseRow _         = Nothing
+safeLast :: [a] -> Maybe a
+safeLast []     = Nothing
+safeLast [x]    = Just x
+safeLast (x:xs) = safeLast xs
 
 {---------------------}
 {- INTERACTIVE SHELL -}
 {---------------------}
 
-data Command
+data Comm
     = Exit
     | Tick
     | Run
@@ -35,36 +29,31 @@ data Command
     | Table String
     | SplitHorizon
 
-parseCommand :: [String] -> Maybe Command
-parseCommand ["exit"] = Just Exit
-parseCommand ["tick"] = Just Tick
-parseCommand ["run"] = Just Run
-parseCommand ["run", i] = RunFor <$> (readMaybe i :: Maybe Int)
-parseCommand ["cost", s, d, i] = Cost s d <$> (readMaybe i :: Maybe Int)
-parseCommand ["fail", s, d] = Just $ Fail s d
-parseCommand ["route", s, d] = Just $ Route s d
-parseCommand ["table", s] = Just $ Table s
-parseCommand ["split-horizon"] = Just SplitHorizon
-parseCommand _ = Nothing
+parseComm :: [String] -> Maybe Comm
+parseComm ["exit"]          = Just Exit
+parseComm ["tick"]          = Just Tick
+parseComm ["run"]           = Just Run
+parseComm ["run", i]        = RunFor <$> (readMaybe i :: Maybe Int)
+parseComm ["cost", s, d, i] = Cost s d <$> (readMaybe i :: Maybe Int)
+parseComm ["fail", s, d]    = Just $ Fail s d
+parseComm ["route", s, d]   = Just $ Route s d
+parseComm ["table", s]      = Just $ Table s
+parseComm ["split-horizon"] = Just SplitHorizon
+parseComm _                 = Nothing
 
-safeLast :: [a] -> Maybe a
-safeLast []     = Nothing
-safeLast [x]    = Just x
-safeLast (x:xs) = safeLast xs
+doComm :: Comm -> (GetDV, Network, State) -> IO (GetDV, Network, State)
+doComm Exit         (f, net, st) = exitSuccess
+doComm Tick         (f, net, st) = pure (f, net, tick f net st)
+doComm Run          (f, net, st) = pure (f, net, fromMaybe st $ safeLast $ run f net st)
+doComm (RunFor n)   (f, net, st) = pure (f, net, fromMaybe st $ safeLast $ runN n f net st)
+doComm (Cost s d n) (f, net, st) = pure (f, alter s d (const n) net, st)
+doComm (Fail s d)   (f, net, st) = pure (f, disconnect s d net, st)
+doComm (Route s d)  (f, net, st) = putStrLn (printRoute (route s d st)) >> pure (f, net, st)
+doComm (Table s)    (f, net, st) = putStrLn (printTable (rtable s st)) >> pure (f, net, st)
+doComm SplitHorizon (_, net, st) = pure (wSH, net, st)
 
-doCommand :: Command -> (GetDV, Network, State) -> IO (GetDV, Network, State)
-doCommand Exit         (f, net, st) = exitSuccess
-doCommand Tick         (f, net, st) = pure (f, net, tick f net st)
-doCommand Run          (f, net, st) = pure (f, net, fromMaybe st $ safeLast $ run f net st)
-doCommand (RunFor n)   (f, net, st) = pure (f, net, fromMaybe st $ safeLast $ runN n f net st)
-doCommand (Cost s d n) (f, net, st) = pure (f, alter s d (const n) net, st)
-doCommand (Fail s d)   (f, net, st) = pure (f, disconnect s d net, st)
-doCommand (Route s d)  (f, net, st) = putStrLn (printRoute (route s d st)) >> pure (f, net, st)
-doCommand (Table s)    (f, net, st) = putStrLn (printTable (rtable s st)) >> pure (f, net, st)
-doCommand SplitHorizon (_, net, st) = pure (wSH, net, st)
-
-commandHelp :: String
-commandHelp = unlines
+commHelp :: String
+commHelp = unlines
     [ "exit             exit the simulation"
     , "tick             advance the simulation by a single tick"
     , "run              run the simulation until state is stable"
@@ -80,13 +69,27 @@ runPrompt :: (GetDV, Network, State) -> IO ()
 runPrompt s = do putStr "> "
                  hFlush stdout
                  tokens <- words <$> getLine
-                 case parseCommand tokens of
-                   Just c -> doCommand c s >>= runPrompt
-                   Nothing -> putStrLn commandHelp >> runPrompt s
+                 case parseComm tokens of
+                   Just c -> doComm c s >>= runPrompt
+                   Nothing -> putStrLn commHelp >> runPrompt s
 
-{-------}
-{- CLI -}
-{-------}
+{------------------------}
+{- NETWORK FILE PARSING -}
+{------------------------}
+
+-- | Parses a network represented as a string
+-- | Links must be split by newlines
+-- | Each link must have format 'alpha beta cost' where 'cost' is an integer
+parseNetwork :: String -> Maybe [(Node, Node, Cost)]
+parseNetwork s = mapM (parseRow . words) (lines s)
+    where parseRow [a, b, c] = case readMaybe c :: Maybe Int of
+                                 Nothing -> Nothing
+                                 Just c' -> Just (a, b, c')
+          parseRow _         = Nothing
+
+{--------------------------}
+{- COMMAND LINE INTERFACE -}
+{--------------------------}
 
 data Configuration = Configuration
     { filepath :: String
@@ -95,25 +98,28 @@ data Configuration = Configuration
 
 configuration :: Parser Configuration
 configuration = Configuration
-    <$> (strOption $ long "file" <> short 'f' <> metavar "FILE")
-    <*> (switch $ long "directed" <> short 'd')
+    <$> (argument auto $ metavar "NETWORK-FILE")
+    <*> (switch $ long "directed" <> short 'd' <> help "Network edges are directed")
 
 opts :: ParserInfo Configuration
 opts = info (configuration <**> helper)
      ( fullDesc
-    <> progDesc "Blah"
-    <> header "bla bla"
+    <> header "Netsim - Uma Zalakain"
+    <> progDesc (unlines
+        [ "Parses file containing network description and jumps into interactive shell."
+        , "Each line in the file represents a link, and must have the following syntax:"
+        , "<SOURCE> <DESTINATION> <COST>"
+        , "where <SOURCE> and <DESTINATION> are arbitrary identifiers and <COST> is an integer."
+        ])
      )
 
-exitWithErr :: String -> Int -> IO ()
-exitWithErr s e = hPutStrLn stderr s >> exitFailure
-
 execute :: Configuration -> IO ()
-execute conf = do ns <- readFile (filepath conf)
-                  case parseNetwork ns of
-                    Nothing -> exitWithErr "Error: could not parse network file" 1
-                    Just n -> do let net = (if directed conf then id else bidir) (unflatten n)
-                                 runPrompt (woSH, net, state net)
+execute conf = do
+    ns <- readFile (filepath conf)
+    case parseNetwork ns of
+      Nothing -> hPutStrLn stderr "Error: could not parse network file" >> exitFailure
+      Just n -> do let net = (if directed conf then id else bidir) (unflatten n)
+                   runPrompt (woSH, net, state net)
 
 main :: IO ()
 main = execute =<< execParser opts
